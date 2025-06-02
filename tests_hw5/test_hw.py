c58e33c1f5e008425c95edb21a335ea00281565c
Natasha Sidorova <natasha771@mail.ru>
2025-06-02 22:37:02 +0300

    unit

--- tests_hw5/test_hw.py
+++ tests_hw5/test_hw.py
@@ -3,5 +3,8 @@ from selene import browser
 
 def test_demoqa():
     browser.open('https://demoqa.com/automation-practice-form')
-    browser.element('firstName').type('Natasha').press_tab()
-    browser.element('lastName').type('Sidorova').press_tab()
+    browser.element('#firstName').type('Natasha').press_tab()
+    browser.element('#lastName').type('Sidorova').press_tab()
+    browser.element('#userEmail').type('natasha771@mail.ru')
+
+

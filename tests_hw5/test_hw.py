2e59d713b05dae334b59de47608895413014f47e
Natasha Sidorova <natasha771@mail.ru>
2025-06-03 12:27:41 +0300

    unit

--- tests_hw5/test_hw.py
+++ tests_hw5/test_hw.py
@@ -6,7 +6,14 @@ def test_demoqa():
     browser.element('#firstName').type('Natasha').press_tab()
     browser.element('#lastName').type('Sidorova').press_tab()
     browser.element('#userEmail').type('natasha771@mail.ru')
-    browser.element('[data-value="Female"]').click
+    browser.element('[for="gender-radio-2"]').click()
     browser.element('#userNumber').type('+79652014545')
-
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('[value="1977"]').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('[value="3"]').click()
+    browser.element('[aria-label="Choose Sunday, April 3rd, 1977"]').click()
+    browser.element('#subjectsInput').type('Kuku').press_enter()
+    browser.element('[for="hobbies-checkbox-1"]').click()
 
